81430c871c1a0ea2696875778d8067a1fc3e5564
Mathias Abeijón <mathiasabeijon@MacBook-Pro-de-Mathias.local>
2026-06-05 10:37:12 -0300

    Agrega clase User con atributos, constructor y getters

---
+++ src/main/java/uy/edu/um/doors/User.java
@@ -0,0 +1,37 @@
+package uy.edu.um.doors;
+
+public class User {
+
+    public enum UserType{
+
+        ADMIN, GENERIC
+    }
+
+    private int uid;
+    private String alias;
+    private UserType type;
+
+    public User(int uid, String alias, UserType type){
+        this.uid = uid;
+        this.alias = alias;
+        this.type = type;
+    }
+
+    public int getUid(){
+        return this.uid;
+    }
+
+    public String getAlias(){
+        return this.alias;
+    }
+
+    public UserType getType(){
+        return this.type;
+    }
+
+    @Override
+    public String toString(){
+        return "USER:" + alias + "UID:" + uid;
+    }
+
+}
